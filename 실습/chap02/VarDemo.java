3e1c4683873149bcbf04031689b3a156e9210aee
whdkqls122 <121709650+whdkqls122@users.noreply.github.com>
2024-03-14 00:56:46 +0900

    Add files via upload

---
+++ 실습/chap02/VarDemo.java
@@ -0,0 +1,24 @@
+package chapter_02;
+
+public class VarDemo {
+	 //   var a =1; var는 메서드 내부에서만 사용 가능
+	   public static void main(String[] args) {
+	      // TODO Auto-generated method stub
+	   
+	      int var = 1; //var를 변수 이름으로 사용 가능
+	      
+	      var x = 1;
+	      
+	      //var x = 1, y = 3, z = 4; var를 사용하겨 다수의 변수를 하나의 실행문으로 초기화할 수 없다.
+	      
+	      //var str = null;  var 변수에 null을 대입할 수 없다.
+	      
+		  //var oops;  var 변수를 바로 초기화 하지 않으면 타입을 추론할 수 없다
+	      //oops = 1;
+		  //	      
+		  //void test(var x) {} 변수 x의 타입을 추론할 수 없다
+				      
+	      
+	   }
+
+	}
