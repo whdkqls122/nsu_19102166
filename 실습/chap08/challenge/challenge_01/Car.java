98d53ae7069a7bb64b54b85f463fe1f990063c39
whdkqls122 <121709650+whdkqls122@users.noreply.github.com>
2024-05-29 02:05:56 +0900

    Add files via upload

---
+++ 실습/chap08/challenge/challenge_01/Car.java
@@ -0,0 +1,12 @@
+package challenge_08_1;
+
+public class Car {
+    private String model;
+
+    public Car (String model){
+        this.model = model;
+    }
+    public String toString() {
+        return model;
+    }
+}
---
+++ 실습/chap08/challenge/challenge_01/CarTest.java
@@ -0,0 +1,13 @@
+package challenge_08_1;
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car myCar = new Car("그랜저");
+        Car yourCar = new Car("그랜저");
+
+        if (myCar.equals(yourCar))
+        System.out.printf("내 자동차는 [%s], 너 자동차는 [%s]로 모델이 같다.", myCar.toString(), yourCar.toString());
+        else
+        System.out.printf("내 자동차는 [%s], 너 자동차는 [%s]로 모델이 다르다.", myCar.toString(), yourCar.toString());
+    }
+}
